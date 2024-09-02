de0735184e8a4cfc59c410b1cced24a00d5b9fae
EthicalRalph <rakinola90@gmail.com>
2024-09-02 03:01:16 +0100

    chore: remove console.log from error helper

--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -3,39 +3,28 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class ErrorHelper {
   static BadRequestException(msg: string | string[]) {
-    console.log(msg);
     throw new HttpException(msg, HttpStatus.BAD_REQUEST);
   }
 
   static UnauthorizedException(msg: string, cause?: Error) {
-    console.log(msg);
-
     throw new HttpException(msg, HttpStatus.UNAUTHORIZED, {
       cause,
     });
   }
 
   static NotFoundException(msg: string) {
-    console.log(msg);
-
     throw new HttpException(msg, HttpStatus.NOT_FOUND);
   }
 
   static ForbiddenException(msg: string) {
-    console.log(msg);
-
     throw new HttpException(msg, HttpStatus.FORBIDDEN);
   }
 
   static InternalServerErrorException(msg: string) {
-    console.log(msg);
-
     throw new HttpException(msg, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   static ConflictException(msg: string) {
-    console.log(msg);
-
     throw new HttpException(msg, HttpStatus.CONFLICT);
   }
 }
